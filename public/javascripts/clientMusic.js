92718265d6a3fe85f1397387a6fed8b1dab32afc
Pedro Eusebio <pedrosantos95@gmail.com>
2016-01-28 22:45:28 -0200

    music is playing, pausing and stopping

--- public/javascripts/clientMusic.js
+++ public/javascripts/clientMusic.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  context = new AudioContext();
-  
-  
+  window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+  var context = new AudioContext();
+  var source = context.createBufferSource();
+  var playing = false;
+  var music = "";
    $('#index_form').submit(function (event) {
     event.preventDefault();
     var data = {};
@@ -10,11 +11,11 @@ $(document).ready(function () {
     
     $.post('/music/path',data, function(result) {
       alert(result.message);
-    })
+    });
   });
   
   $('#button_request').click(function () {
-    if($('.button_play')){
+    if ($('.button_play')) {
       $('#button_request').nextAll('div').remove();
     }
     $.get("/music/loadList", function (data) {
@@ -23,7 +24,9 @@ $(document).ready(function () {
         for (var i = 0; i < data.length; i++ ){
           var name = data[i].split("/");
           name = name[name.length-1];
-          var paragraph = "<div><label class='music_label'>" + name + "</label><input type='button' value='start'  class='button_play' id='"+ name +"'></input></br></div>";
+          var paragraph = "<div><label class='music_label'>" + name.split("%30")[0] + "</label>"+
+          "<input type='button' value='play'  class='button_play' id='"+ name.split('%30')[1] +"'></input>"+
+          "<input type='button' value='stop'  class='button_stop' id='"+ name.split('%30')[1] +"'></input></br></div>";
           $("#music_list").append(paragraph);
         }
       }
@@ -33,22 +36,46 @@ $(document).ready(function () {
   $('#music_list').on("click","input.button_play",function () {
     var data = {};
     var id = $(this).attr('id');
-    console.log(id);
-    data.id = id;
-    console.log(data);
-    $.post('/music/start',data,function (result) {
-      console.log(result);
-    });
-    loadSound();
+    if ($(this).attr('value') == 'play') {
+      if(music != '' && music == id){
+        context.resume();
+      } else {
+        if(music != '') {
+          context.close();
+          $(".button_play#"+ music).attr('value','play');
+        }
+        music = id;
+        data.id = id;
+        $.post('/music/start',data,function (result) {
+          console.log(result);
+        });
+        loadSound();
+      }
+      $(this).attr('value','pause');
+    } else if ($(this).attr('value') == 'pause' && music == id) {
+      context.suspend();
+      $(this).attr('value','play');
+    }
+    
+  });
+  
+  $('#music_list').on("click","input.button_stop", function () {
+    const id = $(this).attr('id');
+    if ( id == music){ 
+      context.close();
+      music = ''; 
+    }
   });
   
   function process(Data) {
+    context = new AudioContext();
     source = context.createBufferSource(); // Create Sound Source
     context.decodeAudioData(Data, function(buffer){
       source.buffer = buffer;
-      source.connect(context.destination); 
+      source.connect(context.destination);
+      //source.noteOn(context.currentTime);
       source.start(context.currentTime);
-    });
+    });    
   };
   
   function loadSound() {
--- routes/music.js
+++ routes/music.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var fs = require('fs');
 var ejs = require('ejs');
 var fpath = require('path');
+var AWS = require('aws-sdk');
 var path = "";
 var music_path = '';
+var musics = new Array();
+
 
 function walk(dir,done) {
   var results = [];
@@ -22,7 +25,11 @@ function walk(dir,done) {
             next();
           });
         } else {
-          results.push(file);
+          var arq = file.split('/');
+          arq = arq[arq.length -1];
+          if(arq.split(".")[1] === "mp3"){
+            results.push(file);
+          }
           next();
         }
       });
@@ -30,6 +37,22 @@ function walk(dir,done) {
   });
   return results;
 };
+
+function listAllKeys(musics){
+  var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+  AWS.config.credentials = credentials;
+  AWS.config.update({region: 'us-east-1'});
+  var s3 =  new AWS.S3();
+  var params = {Bucket: 'musicstreamming'};
+  s3.listObjects(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else{
+      for( var i = 0; i < data.Contents.length; i++){
+        musics.push("s3bucket/" + data.Contents[i].Key);
+      }
+    }
+  });
+}
 
 router.get('/', function (req,res) {
   res.render('music', {title: 'Music Stream'});
@@ -41,15 +64,20 @@ router.get('/loadList', function (req, res) {
     walk(path, function (err, results) {
       if (err) throw err;
       for (var i = 0 ; i < results.length; i++){
-         string_files += results[i] + "%20%";
+         string_files += results[i]+ "%30" + i + "%20%";
+        musics.push(results[i]);
       }
+      /*listAllKeys(musics);
+      console.log(musics);*/
       res.json(string_files);
     });
+    
   }
 });
 
 router.post('/path', function (req,res) {
   var data = req.body.path;
+  musics = [];
   fs.exists(data , function (exists) {
     if(exists){
       path = data;
@@ -63,13 +91,13 @@ router.post('/path', function (req,res) {
 
 router.post('/start', function (req,res) {
   var data = req.body.id;
-  console.log(data);
-  music_path = fpath.join(path,data);
-  console.log(music_path);
+  music_path = musics[data];
+  music_path = music_path.replace(" ", "\ ");
   //music_path = path + "/" + data;
+  res.json("sucess !");
 });
 
-router.get('/play', function (rq,res) {
+router.get('/play', function (req,res) {
   res.set({'Content-Type': 'audio/mpeg'});
   var readStream = fs.createReadStream(music_path);
   readStream.pipe(res);
